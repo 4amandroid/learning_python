a14318783badef4f20bbf238602f488ba11d44bb
Ivan Mitev <4amandroid@gmail.com>
2021-01-09 21:04:59 +0200

    ball movement improve

--- mainsprite.py
+++ mainsprite.py
@@ -7,7 +7,7 @@ from pygame import Surface
 from Config import *
 from Level import Level
  
-FPS = 50
+FPS = 100
 
 #define colors
 WHITE = Color(255, 255, 255)
@@ -35,6 +35,7 @@ class Brick(Sprite):
         self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     
 class Ball(Sprite):
+    
     def __init__(self):
         super().__init__()
         self.x_speed = 2
@@ -55,12 +56,43 @@ class Ball(Sprite):
             self.y_speed *= -1
         if self.rect.left <= 0 or self.rect.right >= SCREEN_WIDTH:
             self.x_speed *= -1    
-        #pygame.sprite.spritecollide(all_balls,all_bricks,True)
-        pass   
-    
-class Colission(Ball,Brick):
-    def __init__(self):
+        #pygame.sprite.spritecollide(ball,all_bricks,True)
+        pass
+
+class Stick(Sprite):
+    def __init__(self) -> None:
         super().__init__()
+        self.image = pygame.image.load(STICK_TEXTURE)
+        self.image = Surface((107,22))
+        self.image = self.image.get_rect() 
+        
+    def update(self):
+        self.image.rect.x = pygame.mouse.get_pos()[0]
+        self.image.rect.y = STICK_Y_POSITION
+        if self.image.rect.right >= SCREEN_WIDTH:
+            self.image.rect.riht = SCREEN_WIDTH - 107
+         
+    pass             
+    
+class Colissions():
+    def __init__(self):
+       
+        self.tolerance = 5
+        
+    def brick_detect(self):    
+        for _brick in all_bricks:
+            if ball.rect.colliderect(_brick.rect):
+                
+                if abs(_brick.rect.top - ball.rect.bottom) < self.tolerance and ball.y_speed > 0:
+                    ball.y_speed *= -1
+                if abs(_brick.rect.bottom - ball.rect.top) < self.tolerance and ball.y_speed < 0:
+                    ball.y_speed *= -1 
+                if abs(_brick.rect.right - ball.rect.left) < self.tolerance and ball.x_speed < 0:  
+                    ball.x_speed *= -1   
+                if abs(_brick.rect.left - ball.rect.right) < self.tolerance and ball.x_speed > 0:  
+                    ball.x_speed *= -1
+                _brick.kill()   
+                 
     pass                
 '''objs = [MyClass() for i in range(10)]
 for obj in objs:
@@ -69,7 +101,8 @@ for obj in objs:
 objs[0].do_sth()'''     
 level=Level()
 level.load(0)
-#print(level.brick_x)      
+#print(level.brick_x)     
+colision = Colissions() 
 all_balls = Group()
 ball = Ball()
 all_balls.add(ball)
@@ -81,10 +114,12 @@ for i in range(level.brick_x.__len__()):
     brick[i].rect.x = level.brick_x[i]
     brick[i].rect.y = level.brick_y[i]
     
-
+stick = Stick()
+all_sticks=Group()
+all_sticks.add(stick)
 # Game loop
 running = True
-tolerance = 5 # ror test
+#tolerance = 5 # ror test
 while running:
     # keep loop running at the right speed
     clock.tick(FPS)
@@ -97,20 +132,24 @@ while running:
             if event.key == pygame.K_q:
                 brick[all_bricks.__len__()-1].kill()
                 brick[all_bricks.__len__()-1].remove()
-    for _brick in all_bricks:
+    colision.brick_detect()            
+    '''for _brick in all_bricks:
         if ball.rect.colliderect(_brick.rect):
             
             if ball.rect.bottom - _brick.rect.top <= tolerance:
                 ball.y_speed *= -1
             if ball.rect.top - _brick.rect.bottom <= tolerance:
                 ball.y_speed *= -1    
-            _brick.kill()   
+            _brick.kill()  ''' 
             
     # Draw / render
     screen.fill(BLACK)
     all_bricks.draw(brick_screen)
     all_balls.draw(screen)
+    
     all_balls.update()
+    #all_sticks.draw(screen)
+    #all_sticks.update()
     # *after* drawing everything, flip the display
     pygame.display.flip()
 
