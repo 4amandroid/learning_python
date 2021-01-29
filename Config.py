f683758944fa8a28e04c1c08482ea82182226404
Ivan Mitev <4amandroid@gmail.com>
2021-01-29 10:17:04 +0200

    images moved in subdirectory

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ SCREEN_HEIGHT = 600
 UP_WALL_Y = 0
 TOP_LEFT_SURFACE = (0,0) 
 BACKGROUND_COLOR = (0, 0, 0)                         
-BRICK_IMAGE = ['brick.png','brick1.png','brick2.png','brick3.png']
+BRICK_IMAGE = ['./images/brick.png','./images/brick1.png','./images/brick2.png','./images/brick3.png']
 SIDE_BORDER_WIDTH = 20
 BORDER_LOCATION = [TOP_LEFT_SURFACE,(SCREEN_WIDTH-SIDE_BORDER_WIDTH, UP_WALL_Y),TOP_LEFT_SURFACE]  
 UP_BORDER_NUMBER = 2
@@ -14,7 +14,7 @@ BALL_X_SPEED = (2, 3)
 BALL_Y_SPEED = (2, 3) 
 DEFAULT_NUMBER_OF_BALLS = 5
 COLISION_TOLERANCE = 4
-STICK_TEXTURE = "greenbar.png"
+STICK_TEXTURE = "./images/greenbar.png"
 
 COLOR_DARK_GREEN = (2, 62, 16)
 COLOR_BLACK = (0,0,0)
