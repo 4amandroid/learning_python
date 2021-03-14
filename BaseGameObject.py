05ae8f5cad325a5540a65269be23f6c143a843c7
Ivan Mitev <4amandroid@gmail.com>
2021-03-14 19:18:49 +0200

    brick to basegameobj

--- BaseGameObject.py
+++ BaseGameObject.py
@@ -15,6 +15,19 @@ class BaseGameObject(Sprite):
         self.rect = self.image.get_rect()
         self.image.fill(COLOR_GREEN)
         self.image.set_colorkey(BACKGROUND_COLOR)
+ class Brick(BaseGameObject):
+        
+    def __init__(self):
+        super().__init__()
+        self.brick_hardness :int= 0
+        self.image = Surface((BRICK_OFFSET_X, BRICK_OFFSET_Y))
+        self.image.set_colorkey(BACKGROUND_COLOR)   
+        self.rect = self.image.get_rect()     
+              
+    def paint(self,brick_hardness:int) -> None:               
+        self.brick_hardness = brick_hardness           
+        self.brick_image = pygame.image.load(BRICK_IMAGE[brick_hardness-1])
+        self.image.blit(self.brick_image,TOP_LEFT_SURFACE)          
 class Ball(BaseGameObject):
     
     def __init__(self) -> None:
